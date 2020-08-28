6efc815dfec5301ba6ef2313ab7e7351245776de
Juvraj Singh Sawhney <juvraj@gmail.com>
2020-08-28 17:01:52 +0100

    Configured session to use filesystem

--- application.py
+++ application.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
+from tempfile import mkdtemp
+
 from models import checkLogin, registerUser
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 app = Flask(__name__)
 
